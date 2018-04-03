ba14272be6baed11b39a0d2204a4d588c3eec0e8
Dea-loC <dealoc@gmail.com>
2018-04-03 18:06:00 +0200

    Ajout Constante et tabbar Custom

---
+++ MonRestoDuMonde/Controller/MonTabBar.swift
@@ -0,0 +1,21 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by Dea-loC on 03/04/2018.
+//  Copyright © 2018 Dea-loC. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+        
+    }
+
+
+}
---
+++ MonRestoDuMonde/Utilitaire/Constante.swift
@@ -0,0 +1,14 @@
+//
+//  Constante.swift
+//  MonRestoDuMonde
+//
+//  Created by Dea-loC on 03/04/2018.
+//  Copyright © 2018 Dea-loC. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
+
