44c3506c49bf911aa23027cee4be580938c88e9a
Dea-loC <dealoc@gmail.com>
2018-04-03 17:56:05 +0200

    Ajout des Controller

---
+++ MonRestoDuMonde/Controller/MenuController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by Dea-loC on 03/04/2018.
+//  Copyright © 2018 Dea-loC. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
