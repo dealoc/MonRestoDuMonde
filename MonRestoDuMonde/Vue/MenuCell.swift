6b0d23cf2d68c37229aa465379278a9974c53da7
Dea-loC <dealoc@gmail.com>
2018-04-03 18:27:24 +0200

    Ajout CollectionView

---
+++ MonRestoDuMonde/Vue/MenuCell.swift
@@ -0,0 +1,15 @@
+//
+//  MenuCell.swift
+//  MonRestoDuMonde
+//
+//  Created by Dea-loC on 03/04/2018.
+//  Copyright © 2018 Dea-loC. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+    
+    
+}
