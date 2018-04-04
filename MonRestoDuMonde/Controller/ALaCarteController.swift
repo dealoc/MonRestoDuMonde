8fba622476a8186c15364f77561d5f2aba76e0cc
Dea-loC <dealoc@gmail.com>
2018-04-04 11:16:44 +0200

    Ajout des sections dans la TableView

--- MonRestoDuMonde/Controller/ALaCarteController.swift
+++ MonRestoDuMonde/Controller/ALaCarteController.swift
@@ -17,7 +17,12 @@ class ALaCarteController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let vue = UIView(frame: tableView.bounds)
+        vue.layer.addSublayer(Degrade())
+        tableView.backgroundView = vue
+        
+        tousLesPlats = LesPlats.obtenir.carteComplete()
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -36,5 +41,24 @@ class ALaCarteController: UITableViewController {
         }
         return UITableViewCell()
     }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let typeDePlat = tousLesPlats[section].type
+        
+        switch typeDePlat {
+        case .entree: return "Nos Entrées"
+        case .platPrincipal: return "Nos Plats Principaux"
+        case .dessert: return "Le Coin des Gourmands"
+        }
+    }
+    
+    
+    
+    
+    
 
 }
