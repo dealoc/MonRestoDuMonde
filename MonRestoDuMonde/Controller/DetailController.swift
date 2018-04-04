f979f177f687b6b1697b442d1f6516907456c4ca
Dea-loC <dealoc@gmail.com>
2018-04-04 11:26:53 +0200

    Ajout detailController

---
+++ MonRestoDuMonde/Controller/DetailController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailController.swift
+//  MonRestoDuMonde
+//
+//  Created by Dea-loC on 04/04/2018.
+//  Copyright © 2018 Dea-loC. All rights reserved.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+
+    @IBOutlet weak var scroll: UIScrollView!
+    
+    var menu: Menu?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.layer.addSublayer(Degrade())
+        view.bringSubview(toFront: scroll)
+        
+        guard menu != nil else { return }
+        miseEnPlace(menu: menu!)
+    }
+
+    func miseEnPlace(menu: Menu) {
+        print("Cela fonctionne on a le menu : \(menu.pays)")
+    }
+
+}
